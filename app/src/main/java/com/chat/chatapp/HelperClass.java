43208ca7ec15590728f954c75772cdcfe8b34f07
Mashnoor <nmmashnoor@gmail.com>
2016-08-23 01:21:57 +0600

    Commit from linux

--- app/src/main/java/com/chat/chatapp/HelperClass.java
+++ app/src/main/java/com/chat/chatapp/HelperClass.java
@@ -10,18 +10,18 @@ public class HelperClass {
 
 
     //Constants
-    final public static String ADD_USER_URL = "http://192.168.0.160/adduser";
+    final public static String ADD_USER_URL = "http://192.168.0.120/adduser";
 
-    final public static String ADD_FRIEND_URL = "http://192.168.0.160/addfriends/";
+    final public static String ADD_FRIEND_URL = "http://192.168.0.120/addfriends/";
 
-    final public static String UPLOAD_FILE = "http://192.168.0.160/addpost/";
-    final public static String FRIENDS_LATEST_POSTS = "http://192.168.0.160/latestposts/";
+    final public static String UPLOAD_FILE = "http://192.168.0.120/addpost/";
+    final public static String FRIENDS_LATEST_POSTS = "http://192.168.0.120/latestposts/";
 
-    final public static String GET_AUDIO_FILE = "http://192.168.0.160/getfile/";
+    final public static String GET_AUDIO_FILE = "http://192.168.0.120/getfile/";
 
-    final public static String GET_IMAGE_FILE = "http://192.168.0.160/getimage/";
+    final public static String GET_IMAGE_FILE = "http://192.168.0.120/getimage/";
 
-    final public static String UPLOAD_IMAGE= "http://192.168.0.160/addimage/";
+    final public static String UPLOAD_IMAGE= "http://192.168.0.120/addimage/";
 
 
     public void saveName(Context context, String name)
