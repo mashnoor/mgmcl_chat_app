7ed5477b0c3440a7cbf3602208067966f0656bce
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-04-13 15:52:55 +0600

    Updated View of user list

--- app/src/main/java/com/origo/android/activities/UserListActivity.java
+++ app/src/main/java/com/origo/android/activities/UserListActivity.java
@@ -1,15 +1,9 @@
 package com.origo.android.activities;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -37,7 +31,7 @@ public class UserListActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buddy_list);
+        setContentView(R.layout.activity_user_list);
         ButterKnife.bind(this);
 
         client = new AsyncHttpClient();
--- app/src/main/java/com/origo/android/adapters/UserListAdapter.java
+++ app/src/main/java/com/origo/android/adapters/UserListAdapter.java
@@ -1,19 +1,20 @@
 package com.origo.android.adapters;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.applozic.mobicomkit.uiwidgets.conversation.ConversationUIService;
 import com.applozic.mobicomkit.uiwidgets.conversation.activity.ConversationActivity;
 import com.origo.android.R;
 import com.origo.android.models.User;
+
+import org.w3c.dom.Text;
 
 public class UserListAdapter extends ArrayAdapter<User>
 {
@@ -34,18 +35,20 @@ public class UserListAdapter extends ArrayAdapter<User>
         final User curr_user = getItem(position);
         if (convertView == null) {
             convertView = activity
-            .getLayoutInflater().inflate(R.layout.buddy_row, parent, false);
+            .getLayoutInflater().inflate(R.layout.user_row, parent, false);
         }
 
 
         TextView buddyName = (TextView) convertView.findViewById(R.id.buddy_name);
+        Button callButton = (Button) convertView.findViewById(R.id.btnCall);
+        Button msgButton = (Button) convertView.findViewById(R.id.btnMessage);
         buddyName.setText(curr_user.getName());
 
         //CircleImageView buddyImage = (CircleImageView) convertView.findViewById(R.id.buddy_image);
         // Glide.with(UserListActivity.this).load(HelperClass.GET_IMAGE_FILE + curr_buddy.getBuddyNumber()).into(buddyImage);
 
 
-        convertView.setOnClickListener(new View.OnClickListener() {
+        msgButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(activity, ConversationActivity.class);
@@ -53,6 +56,15 @@ public class UserListAdapter extends ArrayAdapter<User>
                 intent.putExtra(ConversationUIService.DISPLAY_NAME, curr_user.getName().trim()); //put it for displaying the title.
                 activity.startActivity(intent);
 
+            }
+        });
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:123456789"));
+                activity.startActivity(callIntent);
             }
         });
 
